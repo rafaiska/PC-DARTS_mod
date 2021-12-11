3a918883d5ea0952d0f0cb7b1124fc5d1d7481d2
Rafael <paocomcarnemoidaetubaina@gmail.com>
2021-12-11 12:44:14 -0300

    Adding arch data file

--- scripts/update_scripts_in_sdumont.sh
+++ scripts/update_scripts_in_sdumont.sh
@@ -4,5 +4,5 @@ scp /home/rafael/Projetos/PC-DARTS_mod/my_utils.py rafael.sanchez@login.sdumont.
 scp /home/rafael/Projetos/PC-DARTS_mod/op_oracle.py rafael.sanchez@login.sdumont.lncc.br:/scratch/unicamp-automl/rafael.sanchez/mount/
 scp /home/rafael/Projetos/PC-DARTS_mod/model.py rafael.sanchez@login.sdumont.lncc.br:/scratch/unicamp-automl/rafael.sanchez/mount/
 scp /home/rafael/Projetos/PC-DARTS_mod/genotypes.py rafael.sanchez@login.sdumont.lncc.br:/scratch/unicamp-automl/rafael.sanchez/mount/
-scp /home/rafael/Projetos/PC-DARTS_mod/scripts/pc_darts_mpi_sdumont.cpp rafael.sanchez@login.sdumont.lncc.br:/scratch/unicamp-automl/rafael.sanchez/mount/
+#scp /home/rafael/Projetos/PC-DARTS_mod/scripts/pc_darts_mpi_sdumont.cpp rafael.sanchez@login.sdumont.lncc.br:/scratch/unicamp-automl/rafael.sanchez/mount/
 #scp -r /home/rafael/Projetos/PC-DARTS_mod/thop/thop rafael.sanchez@login.sdumont.lncc.br:/scratch/unicamp-automl/rafael.sanchez/mount/
